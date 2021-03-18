f3e14391e9e759d74ffad8e53684c3b80a2a62d7
adilett02 <adiletturdakunov2002@gmail.com>
2021-03-18 16:11:08 +0600

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+(function($) {
+
+	"use strict";
+
+	$(window).stellar({
+    responsive: true,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: 'scroll'
+  });
+
+
+})(jQuery);
