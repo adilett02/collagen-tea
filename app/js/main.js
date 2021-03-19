3e22a35d9a74b40ac417d3af730a6659b533cf7e
adilett02 <adiletturdakunov2002@gmail.com>
2021-03-19 14:11:59 +0600

    .

--- app/js/main.js
+++ app/js/main.js
@@ -11,5 +11,15 @@
     scrollProperty: 'scroll'
   });
 
+  var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
 
 })(jQuery);
