0e66431f7686a37f2e8b7d9f145349647bd515ce
adilett02 <adiletturdakunov2002@gmail.com>
2021-03-22 14:19:26 +0600

    .

--- app/js/main.js
+++ app/js/main.js
@@ -2,24 +2,6 @@
 
 	"use strict";
 
-	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
-
-  var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
 
 
 })(jQuery);
